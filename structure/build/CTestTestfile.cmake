ebac05e72cd5ed52c01a4962925a2c83f205a924
Gaurav Tiwari <gauravkrtiwari79@gmail.com>
2024-01-11 14:12:17 +0530

    update .gitignore

---
+++ structure/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/yash/repos/sdl2/structure
+# Build directory: /home/yash/repos/sdl2/structure/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
